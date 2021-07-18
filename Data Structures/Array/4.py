d972794a886256b073825b650d88dbafd0570a76
Allen <allenalvin333@gmail.com>
2021-07-18 06:23:53 +0530

    programs

---
+++ Data Structures/Array/4.py
@@ -0,0 +1,37 @@
+# https://www.hackerrank.com/challenges/2d-array/problem
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+import itertools
+
+#
+# Complete the 'hourglassSum' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts 2D_INTEGER_ARRAY arr as parameter.
+#
+
+def hourglassSum(arr):
+    summ = []
+    for j,i in itertools.product(range(0,4),range(0,4)):
+        summ.append(sum(arr[i][j:j+3])+arr[i+1][j+1]+sum(arr[i+2][j:j+3]))
+    return max(summ)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    arr = []
+
+    for _ in range(6):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    result = hourglassSum(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
