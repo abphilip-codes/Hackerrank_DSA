4bca299bfd58d9af188e64f6249d3a3a68e4a331
Allen <allenalvin333@gmail.com>
2021-07-18 07:15:46 +0530

    programs

--- Data Structures/Array/6.py
+++ Data Structures/Array/6.py
@@ -1,1 +1,45 @@
 # https://www.hackerrank.com/challenges/crush/problem
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'arrayManipulation' function below.
+#
+# The function is expected to return a LONG_INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. 2D_INTEGER_ARRAY queries    
+#
+
+def arrayManipulation(n, queries):
+    # Write your code here
+    n1 = [0]*n
+    for a,b,k in queries: 
+        for z in range(a-1,b): n1[z]+=k 
+    return max(n1)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    m = int(first_multiple_input[1])
+
+    queries = []
+
+    for _ in range(m):
+        queries.append(list(map(int, input().rstrip().split())))
+
+    result = arrayManipulation(n, queries)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
