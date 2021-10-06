73df9f9e9cf0b54a82b0fb10024b71487e4074d9
allenalvin333 <allenalvin333@gmail.com>
2021-10-06 10:56:42 +0530

    Simple Array Sum

---
+++ Algorithms/1_Warm_up/2.py
@@ -0,0 +1,19 @@
+# https://www.hackerrank.com/challenges/simple-array-sum/problem
+
+#!/bin/python3
+
+import os
+import sys
+
+def simpleArraySum(ar):
+    sum = 0
+    for i in ar: sum+=i
+    return sum
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    ar_count = int(input())
+    ar = list(map(int, input().rstrip().split()))
+    result = simpleArraySum(ar)
+    fptr.write(str(result) + '\n')
+    fptr.close()
