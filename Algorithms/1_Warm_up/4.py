3d741579cb18cdd1aeb9e3adcfc559b1cf677cef
allenalvin333 <allenalvin333@gmail.com>
2021-10-06 11:04:54 +0530

    A Very Big Sum

---
+++ Algorithms/1_Warm_up/4.py
@@ -0,0 +1,23 @@
+# https://www.hackerrank.com/challenges/a-very-big-sum/problem
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the aVeryBigSum function below.
+def aVeryBigSum(ar):
+    r = 0
+    for z in range(len(ar)): r+=ar[z]
+    return r 
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    ar_count = int(input())
+    ar = list(map(int, input().rstrip().split()))
+    result = aVeryBigSum(ar)
+    fptr.write(str(result) + '\n')
+    fptr.close()
