6412d645453821e846a3295963d8551bb64fc292
allenalvin333 <allenalvin333@gmail.com>
2021-10-06 10:54:59 +0530

    Solve Me First

--- Algorithms/1_Warm_up/1.py
+++ Algorithms/1_Warm_up/1.py
@@ -1,24 +1,8 @@
-# https://www.hackerrank.com/challenges/mini-max-sum/problem
+# https://www.hackerrank.com/challenges/solve-me-first/problem
 
-#!/bin/python3
+def solveMeFirst(a,b): return a+b
 
-import math
-import os
-import random
-import re
-import sys
-
-#
-# Complete the 'miniMaxSum' function below.
-#
-# The function accepts INTEGER_ARRAY arr as parameter.
-#
-
-def miniMaxSum(arr): 
-    print((sum(arr)-(max(arr))),(sum(arr)-(min(arr))))
-
-if __name__ == '__main__':
-
-    arr = list(map(int, input().rstrip().split()))
-
-    miniMaxSum(arr)
+num1 = int(input())
+num2 = int(input())
+res = solveMeFirst(num1,num2)
+print(res)
