586a97fb1dfed5f7f771f7be0dbef2f15f747af5
allenalvin333 <allenalvin333@gmail.com>
2021-10-08 07:50:20 +0530

    Picking Numbers

---
+++ Algorithms/2_Implementation/18.py
@@ -0,0 +1,2 @@
+# https://www.hackerrank.com/challenges/magic-square-forming/problem
+
---
+++ Algorithms/2_Implementation/19.py
@@ -0,0 +1,32 @@
+# https://www.hackerrank.com/challenges/picking-numbers/problem
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'pickingNumbers' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY a as parameter.
+#
+
+def pickingNumbers(a):
+    return max([a.count(z-1)+a.count(z) for z in a])
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    a = list(map(int, input().rstrip().split()))
+
+    result = pickingNumbers(a)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
