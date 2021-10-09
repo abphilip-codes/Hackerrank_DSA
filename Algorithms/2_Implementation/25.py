1c43ab4fd4fa49130afbbea885f169b1912717ee
allenalvin333 <allenalvin333@gmail.com>
2021-10-09 07:45:56 +0530

    Utopian Tree

---
+++ Algorithms/2_Implementation/25.py
@@ -0,0 +1,33 @@
+# https://www.hackerrank.com/challenges/utopian-tree/problem
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'utopianTree' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER n as parameter.
+#
+
+def utopianTree(n):
+    return int("1"*(n//2+1),2) if(n%2==0) else int("1"*((n+1)//2+1),2)-1    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        n = int(input().strip())
+
+        result = utopianTree(n)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
