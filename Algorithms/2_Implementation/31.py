1a0a97ea1482e6ed958e9c69303ba7b161e39445
allenalvin333 <allenalvin333@gmail.com>
2021-10-10 01:18:58 +0530

    Larry's Array

---
+++ Algorithms/2_Implementation/31.py
@@ -0,0 +1,35 @@
+# https://www.hackerrank.com/challenges/larrys-array/problem
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'larrysArray' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts INTEGER_ARRAY A as parameter.
+#
+
+def larrysArray(A):
+    return "YES" if(len([1 for z in range(len(A)) for y in range(z+1,len(A)) if A[z]>A[y]])%2==0) else "NO"
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        n = int(input().strip())
+
+        A = list(map(int, input().rstrip().split()))
+
+        result = larrysArray(A)
+
+        fptr.write(result + '\n')
+
+    fptr.close()
