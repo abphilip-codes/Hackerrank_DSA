95d523da876c72ac340c258159a4b64f9cbcedf5
allenalvin333 <allenalvin333@gmail.com>
2021-10-11 07:57:45 +0530

    Sequence Equation

--- Algorithms/2_Implementation/33.py
+++ Algorithms/2_Implementation/33.py
@@ -1,2 +1,33 @@
 # https://www.hackerrank.com/challenges/permutation-equation/problem
 
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'permutationEquation' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY p as parameter.
+#
+
+def permutationEquation(p):
+    return [p.index(p.index(z+1)+1)+1 for z in range(len(p))]
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    p = list(map(int, input().rstrip().split()))
+
+    result = permutationEquation(p)
+
+    fptr.write('\n'.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
