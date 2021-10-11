387faa96d15d705ae0e74753aec301fa29ae93a2
allenalvin333 <allenalvin333@gmail.com>
2021-10-11 09:29:41 +0530

    Jumping on the Clouds: Revisited

--- Algorithms/2_Implementation/34.py
+++ Algorithms/2_Implementation/34.py
@@ -1,2 +1,30 @@
 # https://www.hackerrank.com/challenges/jumping-on-the-clouds-revisited/problem
 
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the jumpingOnClouds function below.
+def jumpingOnClouds(c, k):
+    return 100-sum(1+2*c[z] for z in range(0,len(c),k)) if(len(c)%k==0) else 80
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    nk = input().split()
+
+    n = int(nk[0])
+
+    k = int(nk[1])
+
+    c = list(map(int, input().rstrip().split()))
+
+    result = jumpingOnClouds(c, k)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
