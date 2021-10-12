0c8a66054bf3ab28c3b223c87f07008eea084631
allenalvin333 <allenalvin333@gmail.com>
2021-10-12 13:21:57 +0530

    Cut the sticks

---
+++ Algorithms/2_Implementation/39.py
@@ -0,0 +1,36 @@
+# https://www.hackerrank.com/challenges/cut-the-sticks/problem
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'cutTheSticks' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def cutTheSticks(arr, ans=[]):
+    while len(arr)>=1:
+        ans.append(len(arr))
+        arr = [z-min(arr) for z in arr if z!=min(arr)]
+    return ans
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = cutTheSticks(arr)
+
+    fptr.write('\n'.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
