be24f7a4ac41382a28b2deda44b97550f018582b
allenalvin333 <allenalvin333@gmail.com>
2021-10-13 06:44:10 +0530

    Equalize the Array

---
+++ Algorithms/2_Implementation/41.py
@@ -0,0 +1,32 @@
+# https://www.hackerrank.com/challenges/equality-in-a-array/problem
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'equalizeArray' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def equalizeArray(arr):
+    return len(arr)-max([arr.count(z) for z in arr])
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = equalizeArray(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
