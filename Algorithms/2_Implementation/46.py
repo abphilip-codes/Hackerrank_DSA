e68403745131233505b1b237360db172e0ab1546
allenalvin333 <allenalvin333@gmail.com>
2021-10-14 07:15:07 +0530

    Encryption

---
+++ Algorithms/2_Implementation/46.py
@@ -0,0 +1,31 @@
+# https://www.hackerrank.com/challenges/encryption/problem
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'encryption' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts STRING s as parameter.
+#
+
+def encryption(s):
+    k = math.ceil(math.sqrt(len(s)))
+    return ' '.join(map(lambda x: s[x::k], range(k)))
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = encryption(s)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
