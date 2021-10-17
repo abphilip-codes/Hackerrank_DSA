490d6a8ba16793557cc19a47925969f5ee24fabb
allenalvin333 <allenalvin333@gmail.com>
2021-10-17 10:02:36 +0530

    Lisa's Workbook

---
+++ Algorithms/2_Implementation/55.py
@@ -0,0 +1,44 @@
+# https://www.hackerrank.com/challenges/lisa-workbook/problem
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'workbook' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER k
+#  3. INTEGER_ARRAY arr
+#
+
+def workbook(n, k, arr, ans=0, p=0):
+    for z in arr:
+        p+=1
+        for y in range(1, z+1):
+            if (y==p): ans+=1
+            if (y%k==0 and y<z): p+=1
+    return ans
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    k = int(first_multiple_input[1])
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = workbook(n, k, arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
