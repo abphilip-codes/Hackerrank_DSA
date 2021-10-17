28bfab9a910afcb7ad0860589f009d1d5fd23931
allenalvin333 <allenalvin333@gmail.com>
2021-10-17 08:52:44 +0530

    Chocolate Feast

---
+++ Algorithms/2_Implementation/53.py
@@ -0,0 +1,42 @@
+# https://www.hackerrank.com/challenges/chocolate-feast/problem
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'chocolateFeast' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER c
+#  3. INTEGER m
+#
+
+def chocolateFeast(n, c, m):
+    return math.floor(n/c) + math.ceil(math.floor(n/c)/(m-1)) - 1
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        first_multiple_input = input().rstrip().split()
+
+        n = int(first_multiple_input[0])
+
+        c = int(first_multiple_input[1])
+
+        m = int(first_multiple_input[2])
+
+        result = chocolateFeast(n, c, m)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
