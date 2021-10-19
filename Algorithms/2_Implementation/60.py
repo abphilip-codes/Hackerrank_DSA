e0f801535c1268635ca0ace7fa03b4be6c5b30a5
allenalvin333 <allenalvin333@gmail.com>
2021-10-19 11:12:32 +0530

    Happy Ladybugs

--- Algorithms/2_Implementation/60.py
+++ Algorithms/2_Implementation/60.py
@@ -16,12 +16,8 @@ import sys
 #
 
 def happyLadybugs(b):
-    for a in set(b):
-        if a != "_" and b.count(a) == 1: return "NO"
-    
-    if b.count("_") == 0:
-        for i in range(1,n-1):
-            if b[i-1]!=b[i] and b[i+1]!=b[i]: return "NO"
+    if(sum([1 for z in set(b) if(z!="_" and b.count(z)==1)])>0): return "NO" 
+    if(sum([1 for z in range(1,len(b)-1) if(b.count("_")==0 and b[z] not in [b[z-1],b[z+1]])])>0): return "NO"
     return "YES"
 
 if __name__ == '__main__':
