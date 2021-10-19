bde524ab45f46aa3422eca69c295cfa245a4f1fb
allenalvin333 <allenalvin333@gmail.com>
2021-10-19 11:04:06 +0530

    Strange Counter

--- Algorithms/2_Implementation/60.py
+++ Algorithms/2_Implementation/60.py
@@ -1,2 +1,41 @@
 # https://www.hackerrank.com/challenges/happy-ladybugs/problem
 
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'happyLadybugs' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts STRING b as parameter.
+#
+
+def happyLadybugs(b):
+    for a in set(b):
+        if a != "_" and b.count(a) == 1: return "NO"
+    
+    if b.count("_") == 0:
+        for i in range(1,n-1):
+            if b[i-1]!=b[i] and b[i+1]!=b[i]: return "NO"
+    return "YES"
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    g = int(input().strip())
+
+    for g_itr in range(g):
+        n = int(input().strip())
+
+        b = input()
+
+        result = happyLadybugs(b)
+
+        fptr.write(result + '\n')
+
+    fptr.close()
---
+++ Algorithms/2_Implementation/61.py
@@ -0,0 +1,30 @@
+# https://www.hackerrank.com/challenges/strange-code/problem
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'strangeCounter' function below.
+#
+# The function is expected to return a LONG_INTEGER.
+# The function accepts LONG_INTEGER t as parameter.
+#
+
+def strangeCounter(t):
+    return 3*2**math.ceil(math.log2(t/3+1))-t-2
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    result = strangeCounter(t)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
