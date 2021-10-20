7b348e7f738e174360413e6b0ffdbc62baa28719
allenalvin333 <allenalvin333@gmail.com>
2021-10-20 08:04:00 +0530

    Absolute Permutation

---
+++ Algorithms/2_Implementation/64.py
@@ -0,0 +1,41 @@
+# https://www.hackerrank.com/challenges/absolute-permutation/problem
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'absolutePermutation' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER k
+#
+
+def absolutePermutation(n, k):
+    if(k==0): return [z+1 for z in range(n)]
+    return [-1] if(n%(2*k)!=0 or 2*k>n) else [(z+1)+k*(1 if (z//k)%2==0 else -1) for z in range(n)] 
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        first_multiple_input = input().rstrip().split()
+
+        n = int(first_multiple_input[0])
+
+        k = int(first_multiple_input[1])
+
+        result = absolutePermutation(n, k)
+
+        fptr.write(' '.join(map(str, result)))
+        fptr.write('\n')
+
+    fptr.close()
