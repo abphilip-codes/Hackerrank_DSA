94577f156d09d58ae646fb97b05cd03453add832
allenalvin333 <allenalvin333@gmail.com>
2021-10-20 07:44:40 +0530

    The Grid Search

---
+++ Algorithms/2_Implementation/63.py
@@ -0,0 +1,60 @@
+# https://www.hackerrank.com/challenges/the-grid-search/problem
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'gridSearch' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts following parameters:
+#  1. STRING_ARRAY G
+#  2. STRING_ARRAY P
+#
+
+def gridSearch(G, P):
+    for z,R in enumerate(G[:-len(P)+1]):
+        for y,C in enumerate(R[:-len(P[0])+1]):
+            if(C==P[0][0] and [G[x][y:y+len(P[0])] for x in range(z,z+len(P))]==P): return "YES"
+    return "NO"
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        first_multiple_input = input().rstrip().split()
+
+        R = int(first_multiple_input[0])
+
+        C = int(first_multiple_input[1])
+
+        G = []
+
+        for _ in range(R):
+            G_item = input()
+            G.append(G_item)
+
+        second_multiple_input = input().rstrip().split()
+
+        r = int(second_multiple_input[0])
+
+        c = int(second_multiple_input[1])
+
+        P = []
+
+        for _ in range(r):
+            P_item = input()
+            P.append(P_item)
+
+        result = gridSearch(G, P)
+
+        fptr.write(result + '\n')
+
+    fptr.close()
