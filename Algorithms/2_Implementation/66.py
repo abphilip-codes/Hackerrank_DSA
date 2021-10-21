2efa85ba7075b9ea8b255d3ac7f245cb260f57eb
allenalvin333 <allenalvin333@gmail.com>
2021-10-21 11:07:03 +0530

    Ema's Supercomputer

--- Algorithms/2_Implementation/66.py
+++ Algorithms/2_Implementation/66.py
@@ -15,36 +15,21 @@ import sys
 # The function accepts STRING_ARRAY grid as parameter.
 #
 
-def twoPluses(grid):
-    for i in range(len(grid)):
-        grid[i] = list(grid[i])
-        grid[i].append('o')
-        grid[i].insert(0,'o')
-    grid.append(['o' for i in range(len(grid[0]))])
-    grid.insert(0,['o' for i in range(len(grid[0]))])
-    res = []
-    for i in range(1,len(grid)-1):
-        for j in range(1, len(grid[0])-1):
-            if grid[i][j]=='G':
-                currCord = []
-                currCord.append((i,j))
-                area = 1
-                start = 1
-                res.append([area,currCord.copy()])
-                while ((grid[i-start][j]=='G') & (grid[i+start][j]=='G') & (grid[i][j-start]=='G') & (grid[i][j+start]=='G')):
-                    currCord.append((i-start,j))
-                    currCord.append((i+start,j))
-                    currCord.append((i,j-start))
-                    currCord.append((i,j+start))
-                    area+=4
-                    start+=1
-                    res.append([area,currCord.copy()])
-    result = 0      
-    for i in range(len(res)-1):
-        for j in range(i+1,len(res)):
-            if (len(set(res[i][1]).intersection(set(res[j][1])))==0) & (res[i][0]*res[j][0]>result):
-                result = res[i][0]*res[j][0]
-    return result
+def twoPluses(grid, r=[]):
+    for z in range(len(grid)): grid[z] = 'o'+grid[z]+'o'
+    grid = ['o'*len(grid[0])]+grid+['o'*len(grid[0])]
+
+    for z in range(1,len(grid)-1):
+        for y in range(1,len(grid[0])-1):
+            if(grid[z][y]=='G'):
+                c,a,s = [(z,y)],1,1
+                r.append([a,c.copy()])
+                while((grid[z-s][y]=='G') and (grid[z+s][y]=='G') and (grid[z][y-s]=='G') and (grid[z][y+s]=='G')):
+                    c.extend([(z-s,y),(z+s,y),(z,y-s),(z,y+s)])
+                    a,s=a+4,s+1
+                    r.append([a,c.copy()])
+        
+    return max([r[z][0]*r[y][0] for z in range(len(r)-1) for y in range(z+1,len(r)) if(len(set(r[z][1]).intersection(set(r[y][1])))==0)])
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
