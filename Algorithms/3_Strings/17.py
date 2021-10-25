4317f1943816b26c4ec9e06e002205de8094d29a
allenalvin333 <allenalvin333@gmail.com>
2021-10-26 00:16:00 +0530

    Gemstones

---
+++ Algorithms/3_Strings/17.py
@@ -0,0 +1,36 @@
+# https://www.hackerrank.com/challenges/gem-stones/problem
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'gemstones' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts STRING_ARRAY arr as parameter.
+#
+
+def gemstones(arr):
+    return len(set.intersection(*[set(arr[z]) for z in range(len(arr))]))
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = []
+
+    for _ in range(n):
+        arr_item = input()
+        arr.append(arr_item)
+
+    result = gemstones(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
