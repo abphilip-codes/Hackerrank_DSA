1732671bbe22d7460dda3aecdac4df6b96760af4
allenalvin333 <allenalvin333@gmail.com>
2021-10-27 14:37:24 +0530

    The Love-Letter Mystery

---
+++ Algorithms/3_Strings/19.py
@@ -0,0 +1,33 @@
+# https://www.hackerrank.com/challenges/the-love-letter-mystery/problem
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'theLoveLetterMystery' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts STRING s as parameter.
+#
+
+def theLoveLetterMystery(s):
+    return sum([abs(ord(s[z])-ord(s[-z-1])) for z in range(len(s)//2)])
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input().strip())
+
+    for q_itr in range(q):
+        s = input()
+
+        result = theLoveLetterMystery(s)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
