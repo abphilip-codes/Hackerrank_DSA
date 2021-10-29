4fc53d0dd97c7050587c3bd939503671557cd8d5
allenalvin333 <allenalvin333@gmail.com>
2021-10-29 06:46:38 +0530

    Game of Thrones - I

--- Algorithms/3_Strings/23.py
+++ Algorithms/3_Strings/23.py
@@ -1,2 +1,30 @@
-# 
+# https://www.hackerrank.com/challenges/game-of-thrones/problem
 
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'gameOfThrones' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts STRING s as parameter.
+#
+
+def gameOfThrones(s):
+    return 'YES' if(len([z for z in set(s) if s.count(z)%2!=0]) < 2) else 'NO'
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = gameOfThrones(s)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
