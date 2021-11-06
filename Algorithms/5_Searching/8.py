2cc16ca2dc344bf9f5290a9dff679fd8ec72995d
allenalvin333 <allenalvin333@gmail.com>
2021-11-06 09:04:17 +0530

    Gridland Metro

---
+++ Algorithms/5_Searching/8.py
@@ -0,0 +1,50 @@
+# https://www.hackerrank.com/challenges/gridland-metro/problem
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'gridlandMetro' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER m
+#  3. INTEGER k
+#  4. 2D_INTEGER_ARRAY track
+#
+
+def gridlandMetro(n, m, k, t):
+    ans = n * m                                         
+    x = a = b = 0
+    for r,z,y in t:                             
+        if(r==x and z-1<b): b = max(b,y)    
+        else: ans,x,a,b = ans-b+a,r,z-1,y                  
+    return ans-b+a    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    m = int(first_multiple_input[1])
+
+    k = int(first_multiple_input[2])
+
+    track = []
+
+    for _ in range(k):
+        track.append(list(map(int, input().rstrip().split())))
+
+    result = gridlandMetro(n, m, k, track)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
